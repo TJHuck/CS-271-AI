17e218b15440335837c37aad16616595ab57a84a
TJHuck <goldensnitch821@gmail.com>
2026-02-04 12:34:02 -0500

    `Updated Albion College Search.py: removed unnecessary code and added new code for graph and image display`

--- Albion College Search.py
+++ Albion College Search.py
@@ -1,18 +1,17 @@
 ## NECCESSARY IMPORTS
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 
-##img = plt.imread("C:/Users/golde/OneDrive/Pictures/Screenshots/ALBION COLLEGE CAMPUS.png")
+img = plt.imread("C:/Users/golde/OneDrive/Pictures/Screenshots/ALBION COLLEGE CAMPUS.png")
 G = nx.Graph()
 
-G.add_node("Node A")
-G.add_node("Node B")
+G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4)])
 
-G.add_edge("Node A", "Node B")
+pos = {1: np.array([0, 0]), 2: np.array([1, 1]), 3: np.array([1, 0]), 4: np.array([2, 1])}
+plt.imshow(img)
 
-##plt.imshow(img)
-
-nx.draw(G, with_labels=True, node_color='skyblue', node_size=1000, edge_color='black', font_size=12)
+nx.draw(G, with_labels=True, node_color='skyblue', node_size=100, edge_color='black', font_size=12)
 
 plt.show()
 ## Locations
