9d31239d7bd131f318619aaa437edee1e27283c7
TJHuck <78052758+TJHuck@users.noreply.github.com>
2026-02-12 14:47:42 -0500

    Added New points, more edges

--- graph.py
+++ graph.py
@@ -10,19 +10,22 @@ img = cv2.imread("collegemap.png")
 
 
 ## manually add to nodes. "P" for sidewalk intersections that arent buildings
-nodes = [["Wesley Hall",400,200],["Goodrich Chapel",400,440],["P1",400,560],
-         ["Observatory",380,580],["P2",340,635],["Stockwell/Mudd Library",280,635],
-         ["Olin Hall",275,720],["P3",400,535],["P4",400,285],["P5",400,640],
+nodes = [["Wesley Hall",400,200],["Goodrich Chapel",400,440],["Observatory Crosswalk",400,560],
+         ["Observatory",380,580],["Library Accessibility Ramp",340,635],["Stockwell/Mudd Library",280,635],
+         ["Olin Hall",275,720],["Chapel Corner Crosswalk",400,535],["Wesley Chapel Side Crosswalk",400,285],
+         ["Quad Streetlamp Intersection",400,640],["Wesley Hall Side Crosswalk",400,245],
          ["Kresge Gymnasium", 180, 680],["Tau Kappa Epsilon",750,850],
-         ["Delta Sigma Phi",645,855],["Sigma Chi",650,920],["P6",417,730],
-         ["P7",483,640],["P8",483,600],["P9",445,535],["P10",443,555],["P11",565,635],
-         ["P12",565,725]]
+         ["Delta Sigma Phi",645,855],["Sigma Chi",650,920],["Ferguson Circular Intersection",417,730],
+         ["The Rock",483,640],["Vulgamore Quad Entrance",483,600],["Fiske House Corner Crosswalk",445,535],
+         ["Quad Fiske House Crosswalk",443,555],["Vulgamore Robinson Front Entrance Intersection",565,635],
+         ["Robinson Kellog Front Entrance Intersection",565,725],["Alpha Tau Omega",745,920],["Sigma Nu",785,920],
+         ["Delta Tau Delta",790,855],["Fraternity Alleyway",780,885]]
 
 for node in nodes:
     cv2.circle(img,(node[1],node[2]),6,(0,0,255),-1)
 
 ## manually add to edges. [index of point1 in nodes, index of point2 in nodes]
-edges = [[0,8]]
+edges = [[0,10],[10,8],[8,2],[2,7],[7,2],[2,3],[3,4],[4,5]]
 for edge in edges:
     point1 = (nodes[edge[0]][1],nodes[edge[0]][2])
     point2 = (nodes[edge[1]][1],nodes[edge[1]][2])
@@ -35,9 +38,9 @@ for edge in edges:
     
 print(edges)
 ## show image
-cv2.imshow('Map', img) ## <--- This is the way Nathan had to display the original image, below is just the way I'm scaling it to fit my screen
-##resized = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-##cv2.imshow('Resized', resized)
+## cv2.imshow('Map', img) ## <--- This is the way Nathan had to display the original image, below is just the way I'm scaling it to fit my screen
+resized = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+cv2.imshow('Resized', resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
