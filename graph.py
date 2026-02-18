08fea7f7629fb120f7a3e671ed027fce5fb7cfdc
TJHuck <78052758+TJHuck@users.noreply.github.com>
2026-02-17 23:35:56 -0500

    Final Product as of 2/17 @ 11:34 PM. Ready to turn in, works as intended.

--- graph.py
+++ graph.py
@@ -10,88 +10,12 @@ yardPerPixel = 0.667
 ## load image
 img = cv2.imread("collegemap.png")
 
-
-## manually add to nodes. "P" for sidewalk intersections that arent buildings
-nodes = nodes = [["Wesley Hall",400,200],["Goodrich Chapel",400,440],["Observatory Crosswalk",400,560],
-                ["Observatory",380,580],["Library Accessibility Ramp",340,635],["Stockwell/Mudd Library",280,635],
-                ["Olin Hall",275,720],["Chapel Corner Crosswalk",400,535],["Wesley Chapel Side Crosswalk",400,285],
-                ["Quad Streetlamp Intersection",400,640],["Wesley Hall Side Crosswalk",400,245],
-                ["Kresge Gymnasium", 180, 680],["Tau Kappa Epsilon",750,850],
-                ["Delta Sigma Phi",645,855],["Sigma Chi",650,920],["Ferguson Circular Intersection",417,730],
-                ["The Rock",483,640],["Vulgamore Hall",483,600],["Fiske House Corner Crosswalk",445,535],
-                ["Quad Fiske House Crosswalk",443,555],["Vulgamore Robinson Front Entrance Intersection",565,635],
-                ["Robinson Kellog Front Entrance Intersection",565,725],["Alpha Tau Omega",745,920],["Sigma Nu",785,920],
-                ["Delta Tau Delta",790,855],["Fraternity Alleyway",780,885],["Robinson Hall",565,680],["Kellog Center",550,755],["Ferguson",417,760],
-                ["Library Sidewalk",240,640],["Center Of Quad",315,675],["Robinson Hall Back Entrance",483,685],["Science Complex Crosswalk",605,285],
-                ["Sidewalk",605,440],["Science Complex",650,430],["Bobbit Visual Arts Croner Crosswalk",610,535],["Robinson Crosswalk", 605,680],["Baldwin Crosswalk", 635,680],
-                ["Baldwin Front ENtrance", 665,675],["Vulgamore Front Entrance Circular Intersection",585,600],["Vulgamore Front Entrance Corner Crosswalk",610,556],
-                ["Bobbit Visual Arts Crosswalk",530,535],["Vulgarmore Side Crosswalk",530,557],["Seaton Hall Front Corner Crosswalk",635,556],
-                ["Seaton Hall Front Entrance",715,556],["Mitchell Towers Circular Intersection",865,675],["Mitchell Towers Front Entrance", 925,675]]
-
-
-
-
-##comment out when all nodes and edges added
-########################################
-for i,node in enumerate(nodes):
-    cv2.circle(img,(node[1],node[2]),6,(0,0,255),-1)
-    cv2.putText(img,str(i), (node[1]+5,node[2]+5),1, 1.5, 255,2)
-########################################
-
-
-    
-## manually add to edges. [index of point1 in nodes, index of point2 in nodes]
-edges = [[0,10],[10,8],[8,1],[1,7],[2,7],[2,3],[3,4],[4,5],[7,18],[2,19],[18,19],[19,17],[17,16],[16,9],[9,2],[9,4],
-         [4,30],[6,11],[29,5],[29,6],[29,11],[9,15],[15,28],[15,6],[15,21],[28,27],[30,6],[29,30],[15,30],[16,31],
-         [31,15],[16,20],[20,26],[26,21],[21,27],[32,8],[32,33],[1,33],[33,34],[35,33],[26,36],[36,37],[37,38],[20,39],
-         [39,40],[40,35],[18,41],[19,42],[41,35],[42,40],[41,42],[42,39],[40,43],[43,44],[38,45],[45,46]]
-
-
-
-for edge in edges:
-    point1 = (nodes[edge[0]][1],nodes[edge[0]][2])
-    point2 = (nodes[edge[1]][1],nodes[edge[1]][2])
-    cv2.line(img,point1,point2,(255,0,0),3) ## comment this line out when all nodes and edges are added
-    (x1,y1)=point1
-    (x2,y2)=point2
-    dist = math.sqrt((x1-x2)**2 + (y1-y2)**2)
-    edge.append(dist*yardPerPixel)
-
-
-
-##comment out when all nodes and edges added
-########################################
-## show image
-resized = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-cv2.imshow('Resized', resized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-########################################
-
-
-
-
-"""
-## make graph
-graph = nx.Graph()
-img = cv2.imread("collegemap.png")
-## add nodes from previous work to graph
-for i,node in enumerate(nodes):
-    graph.add_node(i,pos=(node[1],node[2]),name=node[0])
-
-## add edges from previous work to graph
-for edge in edges:
-    graph.add_edge(edge[0],edge[1],weight=edge[2])
-
-
-
-
-## begin user input
-for node in graph.nodes:
-    print(str(node)+": "+graph.nodes[node]['name'])
-print("To select a location input the number before. Example: 0:Wesley Hall. ENTER 0")
-start=int(input("Please enter your starting location number: "))
-end=int(input("Please enter your final location number: "))
+### Defined Functions ################################################################
+def find_department(dept_name):
+    for node in graph.nodes:
+        if dept_name.lower() in [d.lower() for d in graph.nodes[node]["departments"]]:
+            return node
+    return None
 
 ## A star algorithm
 def heuristic(a,b):
@@ -132,11 +56,113 @@ def astar(graph,start,end):
             weightQueue.append(w+heuristic(node,children))
             pathPlus = path+[children]
             pathQueue.append(pathPlus)
-            
-        
-        
-        
+######################################################################################
+
+## manually add to nodes. "P" for sidewalk intersections that arent buildings
+nodes = [["Wesley Hall",400,200],["Goodrich Chapel",400,440,["Music Dept"]],["Observatory Crosswalk",400,560],
+        ["Observatory",380,580],["Library Accessibility Ramp",340,635],["Stockwell/Mudd Library",280,635,["Cutler Center","Writing Center"]],
+        ["Olin Hall",275,720,["Communications Dept", "Psychological Science Dept"]],["Chapel Corner Crosswalk",400,535],
+        ["Wesley Chapel Side Crosswalk",400,285],["Quad Streetlamp Intersection",400,640],["Wesley Hall Side Crosswalk",400,245],
+        ["Kresge Gymnasium", 180, 680],["Tau Kappa Epsilon",750,850],["Delta Sigma Phi",645,855],["Sigma Chi",650,920],["Ferguson Circular Intersection",417,730],
+        ["The Rock",483,640],["Vulgamore Hall",483,600,["Philosophy Dept","Language and Culture Dept", "Political Science Dept", "Religious Studies Dept"]],["Fiske House Corner Crosswalk",445,535],
+        ["Quad Fiske House Crosswalk",443,555],["Vulgamore Robinson Front Entrance Intersection",565,635],["Robinson Kellog Front Entrance Intersection",565,725],["Alpha Tau Omega",745,920],["Sigma Nu",785,920],
+        ["Delta Tau Delta",790,855],["Fraternity Alleyway",780,885],["Robinson Hall",565,680,["Anthropology and Sociology Dept",""]],["Kellog Center",550,755,["Bookstore","Qdoba",
+        "Office of Fraternity and Sorority Life", "Campus Post Office"]],["Ferguson",417,760,["IT Helpesk", "Registrar's Office","Academic Affairs Office","Financial Aid Office", "Title IX"]],
+        ["Library Sidewalk",240,640],["Center Of Quad",315,675],["Robinson Hall Back Entrance",483,685],["Science Complex Crosswalk",605,285],
+        ["Sidewalk",605,440],["Science Complex",650,430,["Math and Computer Science Dept", "Science Complex Front Office", "Norris","Palenske","Putnam","Kresge","Astronomy and Physics Department","Chemistry","Biology",
+        "Earth and Environment Dept", "Kinesiology Dept"]],["Bobbit Visual Arts Corner Crosswalk",610,535],["Robinson Crosswalk", 605,680],["Baldwin Crosswalk", 635,680],
+        ["Baldwin Front ENtrance", 665,675],["Vulgamore Front Entrance Circular Intersection",585,600],["Vulgamore Front Entrance Corner Crosswalk",610,556],
+        ["Bobbit Visual Arts Center",530,535,["Art and Art History Dept"]],["Vulgarmore Side Crosswalk",530,557],["Seaton Hall Front Corner Crosswalk",635,556],
+        ["Seaton Hall Front Entrance",715,556],["Mitchell Towers Circular Intersection",865,675],["Mitchell Towers Front Entrance", 925,675],
+        ["Whitehouse Side Entrance",710,780],["Whitehouse Side Entrance, Sidewalk Entrance",710,810],["Whitehouse Corner Sidewalk",635,810],
+        ["Fraternity Row Corner Crosswalk",635,830],["Sigma Chi Corner Crosswalk",635,950],["Athletics Complex Crosswalk",635,1025],["Tennis Courts",710,1200],
+        ["Athletics Complex Crosswalk Fork",710,1100],["Serra Fitness Center Parking",840,1110],["Serra Fitness Center Entrance",780,1150,["Field House","Theatre","Theatre Dept"]],
+        ["Tennis Court Corner",710,1260],["Alumni Field",800,1260,["Football Field","Track and Field"]],["Athletics Fields Fork",1000,1260],
+        ["LAX Field",1110,1220],["Soccer Field",1110,1300],["Softball Field",1280,1300],["Baseball Field",1280,1220],
+        ["KC Parking Lot Sidewalk Intersection",540,835],["Front Frat Row Crosswalk",600,835],["KC Sidewalk Intersection Crosswalk",600,800],
+        ["KC Intersection #2",525,795],["Mae Appartment Entrance 1",910,945],["Mae Appartment Entrance 2",945,945],["Mae Appartment Entrance 3",1000,945],
+        ["Whitehouse Back Corner Crosswalk",785,810],["Fraternity Alley Exit",780,840]]
+
+
+
+
+##comment out when all nodes and edges added
+"""
+######################################## ALSO PURELY FOR TESTING AND WORKING PURPOSES. NOT NEEDED IN FINAL PRODUCT
+for i,node in enumerate(nodes):
+    cv2.circle(img,(node[1],node[2]),6,(0,0,255),-1)
+    cv2.putText(img,str(i), (node[1]+5,node[2]+5),1, 1.5, 255,2)
+########################################
+"""
+
+
     
+## manually add to edges. [index of point1 in nodes, index of point2 in nodes]
+edges = [[0,10],[10,8],[8,1],[1,7],[2,7],[2,3],[3,4],[4,5],[7,18],[2,19],[18,19],[19,17],[17,16],[16,9],[9,2],[9,4],
+         [4,30],[6,11],[29,5],[29,6],[29,11],[9,15],[15,28],[15,6],[15,21],[28,27],[30,6],[29,30],[15,30],[16,31],
+         [31,15],[16,20],[20,26],[26,21],[21,27],[32,8],[32,33],[1,33],[33,34],[35,33],[26,36],[36,37],[37,38],[20,39],
+         [39,40],[40,35],[18,41],[19,42],[41,35],[42,40],[41,42],[42,39],[40,43],[43,44],[38,45],[45,46],[28,67],[21,66],
+         [27,66],[67,66],[64,67],[64,65],[65,66],[66,49],[65,50],[49,50],[49,48],[48,47],[48,71],[50,13],[50,14],[50,51],
+         [51,14],[51,13],[51,52],[51,68],[68,69],[69,70],[52,54],[54,55],[54,56],[54,53],[53,57],[57,58],[58,59],[59,60],
+         [59,61],[61,62],[62,63],[72,50],[22,25],[23,25],[25,72],[12,50],[24,72]]
+
+
+## Creates Edges
+for edge in edges:
+    point1 = (nodes[edge[0]][1],nodes[edge[0]][2])
+    point2 = (nodes[edge[1]][1],nodes[edge[1]][2])
+    ##cv2.line(img,point1,point2,(255,0,0),3) ## comment this line out when all nodes and edges are added
+    (x1,y1)=point1
+    (x2,y2)=point2
+    dist = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+    edge.append(dist*yardPerPixel)
+
+
+"""
+##comment out when all nodes and edges added THIS SECTION IS PURELY FOR TESTING AND WORKING PURPOSES. IT IS NOT NEEDED IN THE FINAL PRODUCT
+########################################
+## show image
+resized = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+cv2.imshow('Resized', resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+########################################
+"""
+
+## make graph
+graph = nx.Graph()
+img = cv2.imread("collegemap.png")
+## add nodes from previous work to graph
+for i,node in enumerate(nodes):
+    departments = node[3] if len(node) > 3 else []
+    graph.add_node(i,
+                   pos=(node[1],node[2]),
+                   name=node[0],
+                   departments=departments) 
+## add edges from previous work to graph
+for edge in edges:
+    graph.add_edge(edge[0],edge[1],weight=edge[2])
+
+
+
+## begin user input
+for node in graph.nodes:
+    print(str(node)+": "+graph.nodes[node]['name'])
+print("To select a location input the number before. Example: 0:Wesley Hall. ENTER 0")
+start=int(input("Please enter your starting location number: "))
+
+choice = input("Search by (1) Building Number or (2) Department? ")
+
+if choice == "1":
+    end=int(input("Please enter destination number:"))
+    
+elif choice == "2":
+    dept_name = input("Please enter department name: ")
+    end = find_department(dept_name)
+    
+    if end is None:
+        print("Department not found.")
+        exit()
 
 ## we probably cant use the built in astar but idk
 ##finalPath = nx.astar_path(graph,start,end,heuristic=heuristic,weight="weight")
@@ -154,7 +180,3 @@ resized = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 cv2.imshow('Resized', resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-"""
-    
-    
